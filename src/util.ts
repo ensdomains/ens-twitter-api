80a838b4d02b6ac075eb8134f1555ab6a237d71a
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-02-02 18:53:47 +0000

    Add hour

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
 export const HOUR = 1
 export const GRACE_PERIOD = 90
 export const DECAY_PERIOD = 28
+const moment = require('moment')
+
 export function formatDate(date){
-  return date.utc().format('MMMM Do YYYY, h:mm:ss a (UTC)')
+  return date.utc().format('MMMM Do YYYY ha UTC')
 }
 
 export function formatShortDate(data){
@@ -14,7 +16,7 @@ export const pluralize = (num) => {
 }
 
 export function generateSummary(verb, length) {
-  return `${length} .eth name${ pluralize(length) } ${ length === 1 ? 'has' : 'have' } been ${verb} in the last hour #ens${verb}`
+  return `${length} .eth name${ pluralize(length) } ${ length === 1 ? 'has' : 'have' } been ${verb} in the last hour (${formatDate(moment)}) #ens${verb}`
 }
 
 export function parser(input){
